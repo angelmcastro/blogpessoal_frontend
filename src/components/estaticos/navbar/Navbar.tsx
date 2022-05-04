83e98ee662cbb8e6428ec5264190dfdcf91b760f
angelmcastro <100306153+angelmcastro@users.noreply.github.com>
2022-05-03 21:35:57 -0300

    atualizando

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Navbar.css';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { color } from '@mui/system';
+
+
+
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box style={{ cursor: "pointer" }} >
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+
+                        <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1} style={{ cursor: "pointer", color: 'white'}}>
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
+                        </Link>
+                        
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
